78b5704f650263d5f180142dbd95fa5777955d03
LuckyMonkey809 <kevanmok@speedburst.com>
2026-09-18 19:22:58 -0700

    Goddy changes

--- Scripts/game_controller.gd
+++ Scripts/game_controller.gd
@@ -1,26 +1,91 @@
 extends Node2D
-@export var platform16: PackedScene
-@export var platform32: PackedScene
-@export var platform48: PackedScene
+
+#Small,medium,large platform
+@export var sPf: PackedScene
+@export var mPf: PackedScene
+@export var lPf: PackedScene
 @export var character: PackedScene
 
 @export var platSpawnRate = 0.0
-@export var platMinDistance = 
+@export var platMinDistanceRatio = 0.5
 
+#These are the difficulty proportions of the total y distance the character needs to jump to reach the next platform
+#Not to be changed except by game devs 
+@export var easyRatio = 0.4
+@export var mediumRatio = 0.6
+@export var hardRatio = 0.8
+
+#Idle Time for when platform doesn't spawn
+var idTiS1 = 0.0
+var idTiM1 = 0.0
+var idTiL1 = 0.0
+
+#Dynamic proportions, changed by the game
+var spawnRateS1: float
+var spawnRateM1: float
+var spawnRateL1: float
+@export var platSpeed = 20
+
+#Dimensions of the spawnbox outside of the camera
+var minX = 110
+var maxX = 210
+var maxY = -118
+var minY = -202
+
+var timeToJumpHeight: float
+var maxJumpHeight: float 
+var platydistance: float
+var totalydistance: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	var mMan = character.instantiate()
+	mMan.position = Vector2(-2000, 2000)
+	
 	# The max jump height of the mountain man by himself
-	var timeToJumpHeight = character.JUMP_VELOCITY/character.get_gravity()
-	var maxJumpHeight = 2 * character.JUMP_VELOCITY * character.JUMP_VELOCITY / character.get_gravity()
+	timeToJumpHeight = mMan.JUMP_VELOCITY/980
+	maxJumpHeight = 2 * mMan.JUMP_VELOCITY * mMan.JUMP_VELOCITY / 980
 
 # The y distance platforms move enroute --> MAKE SURE TO ADD SPEED MULTIPLIER
-	var platydistance = platform16.speed * timeToJumpHeight
-	var totalydistance = platydistance + maxJumpHeight
+	platydistance = platSpeed * timeToJumpHeight
+	
+# The total y distance character moves relative to the platform
+	totalydistance = platydistance + maxJumpHeight
+	
+	spawnRateS1 = easyRatio
+	spawnRateM1 = easyRatio
+	spawnRateL1 = easyRatio
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	pass
+	idTiS1 += delta
+	#if randf() < spawnRateS1:
+		#spawnS1()
+	#spawnRateS1 += 0.01 * delta
+	
+	# character can jump 8 instances of time to jump height spawn rate
+	if (-timeToJumpHeight) <= idTiS1:
+		idTiS1 = 0
+		spawnRateS1 = 0
+		var instanceSPf = sPf.instantiate()
+		instanceSPf.position = Vector2(maxX, minY)
+		add_child(instanceSPf)
+		pass
+	
+
+func randLocation() -> Vector2:
+	var rand_x = round(randf_range(minX, maxX))
+	var rand_y = round(randf_range(maxX, maxY))
+	return Vector2(rand_x, rand_y)
+
+func spawnS1():
+	idTiS1 = 0
+	spawnRateS1 = 0
+	var instanceSPf = sPf.instantiate()
+	instanceSPf.position = randLocation()
+	# print(instanceSPf.position)
+	add_child(instanceSPf)
--- Scripts/platform_16x_16.gd
+++ Scripts/platform_16x_16.gd
@@ -1,10 +1,19 @@
 extends AnimatableBody2D
-@export var speed = 20
+var game = get_parent()
+
+var speed = 20
 
 func _ready() -> void:
+	var game = get_parent()
+	speed = game.platSpeed
 	pass
 
 func _physics_process(delta: float) -> void:
 	move_local_x(-speed *delta)
 	move_local_y(speed *delta)
 	pass
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
+	pass # Replace with function body.
